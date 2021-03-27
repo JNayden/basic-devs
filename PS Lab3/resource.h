0614037743704124f252da68abf9f57da9dddd58
Nayden <naiden452@gmail.com>
2021-03-27 22:44:29 +0200

    Add project files.

---
+++ PS Lab3/resource.h
@@ -0,0 +1,48 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by PS Lab3.rc
+//
+#define IDC_MYICON                      2
+#define IDD_PSLAB3_DIALOG               102
+#define IDS_APP_TITLE                   103
+#define IDD_ABOUTBOX                    103
+#define IDM_ABOUT                       104
+#define IDM_EXIT                        105
+#define IDI_PSLAB3                      107
+#define IDI_SMALL                       108
+#define IDC_PSLAB3                      109
+#define IDR_MAINFRAME                   128
+#define IDD_MTEST                       129
+#define IDC_A                           1000
+#define IDC_C                           1001
+#define IDC_B                           1002
+#define IDC_RANDOM                      1003
+#define IDC_CHECK                       1004
+#define IDC_CLEAR                       1005
+#define IDC_FINISH                      1006
+#define IDC_WRONG                       1007
+#define IDC_CORRECT                     1008
+#define IDC_PLUS                        1009
+#define IDC_MINUS                       1010
+#define IDC_MULTIPLY                    1011
+#define IDC_DIVIDE                      1012
+#define IDC_R20                         1013
+#define IDC_R50                         1014
+#define IDC_R100                        1015
+#define IDC_LIST1                       1016
+#define IDC_LIST                        1016
+#define ID_TOOLS_MATHTEST               32771
+#define IDM_MTEST                       32772
+#define IDC_STATIC                      -1
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NO_MFC                     1
+#define _APS_NEXT_RESOURCE_VALUE        130
+#define _APS_NEXT_COMMAND_VALUE         32773
+#define _APS_NEXT_CONTROL_VALUE         1017
+#define _APS_NEXT_SYMED_VALUE           110
+#endif
+#endif
